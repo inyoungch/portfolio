d0648b2de8231a80cc87c51bc132b3cd733851a7
In Young Choi <inkatsoy08@gmail.com>
2018-01-15 03:10:45 +0600

    hm3 parts

--- src/scripts/common/hamburger.js
+++ src/scripts/common/hamburger.js
@@ -1,42 +1,21 @@
-//Hamburger menu
-let hamburger = (function() {
-
-    let button = document.querySelector('#hamburger__menu-link');
-    let menu = document.querySelector('#dark-menu');
-    let body = document.querySelector('body');
-    let itemsList = document.getElementById("nav__list_hamburger").children;
-    itemsList = Array.prototype.slice.call(itemsList);
-
-    let _toggleMenu = function(e) {
-        button.classList.toggle('is-active');
-        menu.classList.toggle('is-active');
-        body.classList.toggle('locked');
+(function hamb() {
+ 
+    "use strict";
+   
+    var toggles = document.querySelectorAll(".cmn-toggle-switch");
+   
+    for (var i = toggles.length - 1; i >= 0; i--) {
+      var toggle = toggles[i];
+      toggleHandler(toggle);
     };
+   
+    function toggleHandler(toggle) {
+      toggle.addEventListener( "click", function(e) {
+        e.preventDefault();
+        (this.classList.contains("active") === true) ? this.classList.remove("active") : this.classList.add("active");
+      });
+    }
+   
+  })();
 
-    var closeMenu = function closeMenu() {
-        button.classList.remove('is-active');
-        menu.classList.remove('is-active');
-        body.classList.remove('locked');
-    };
-
-    let addListeners = function() {
-        button.addEventListener('click', _toggleMenu);
-
-        for(i=0 ; i<itemsList.length ; i++){
-                itemsList[i].addEventListener('click',closeMenu);
-            }
-    };
-
-    document.addEventListener('keydown', function(e) {
-        if (e.keyCode == 27) closeMenu();
-    });
-
-    return {
-        init: addListeners
-    };
-})({
-    button: '#hamburger__menu-link',
-    menu: '#dark-menu'
-});
-
-hamburger.init();
+  module.exports=hamb;
